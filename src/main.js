67f1cc725f32e4d5bf696fa8b200858f1998dfd0
pumba-dev <dudu32769144@gmail.com>
2022-01-27 22:01:19 -0300

    Config Vue-Virtualized-List

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import Root from './App.vue'
 import router from './router'
 import SuiVue from 'semantic-ui-vue';
 import VueTheMask from 'vue-the-mask'
+import VirtualizedList from "vue-virtualized-list";
 
 // Create Vue App
 const App = createApp(Root)
 
 // Plugins
+App.component("virtualized-list", VirtualizedList)
 App.use(router)
 App.use(SuiVue);
 App.use(VueTheMask)
